a77cfa7984492d5b2b7bb0e248d25e99a0629a3a
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 21:06:05 +0200

    init

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_04._07
 {
     public class StringTools
     {
-        public string Reverse(string original)
-        {
-            throw new NotImplementedException();
-        }
+        public string Reverse(string original) => string.Join("", original.Reverse());
+        
     }
 }
